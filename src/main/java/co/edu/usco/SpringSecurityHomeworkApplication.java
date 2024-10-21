5ed257b8431c58a3f40df0e7659457bd66b5c700
Julian Enrique <julian.buitrago3003@gmail.com>
2024-10-21 10:45:43 -0500

    initial commit

---
+++ src/main/java/co/edu/usco/SpringSecurityHomeworkApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/usco/SpringSecurityHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
